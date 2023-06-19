9bd68213c65ebb7804551e63c2c0e9cd685bd416
TitoAlcivar <tjal2002@outlook.esm>
2023-06-19 21:01:52 +0200

    Pe

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const list = async (url) => {
     const res = await fetch(url)
     const data = await res.json()
     return data 
-    
+
 }
 const form = document.querySelector('#movieapp')
 
 form.addEventListener('submit', async (event)  => {
     event.preventDefault();
     const movie = document.querySelector('#cityInput');
-    console.log( await (list(`https://api.themoviedb.org/3/search/movie?api_key=fdff2360f3ab6fe1750a9be6ebab1eba&query=${movie.value}`)));
+    console.log(await (list(`https://api.themoviedb.org/3/search/movie?api_key=fdff2360f3ab6fe1750a9be6ebab1eba&query=${movie.value}`)));
 });
 
 
