3f18f2510059ca7ab5589601a6d00f7339d52f66
TitoAlcivar <tjal2002@outlook.es+>
2023-06-20 23:32:50 +0200

    Rediseñando el js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-const list = async (url) => { 
-    const res = await fetch(url)
-    const data = await res.json()
-    return data 
-
-}
 const form = document.querySelector('#movieapp')
 
-form.addEventListener('submit', async (event)  => {
-    event.preventDefault();
-    const movie = document.querySelector('#cityInput');
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    const input = document.querySelector('#id')
+
+})
+
     console.log(await (list(`https://api.themoviedb.org/3/search/movie?api_key=fdff2360f3ab6fe1750a9be6ebab1eba&query=${movie.value}`)));
-});
 
 
 
