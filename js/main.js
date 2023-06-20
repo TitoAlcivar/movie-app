e7c3477155ee8e03c940375d0ab959bf4881a07a
TitoAlcivar <tjal2002@outlook.es+>
2023-06-21 00:00:57 +0200

    Nuevo Js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const form = document.querySelector('#movieapp')
+const card = document.querySelector('#movie')
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
     event.preventDefault(); 
     const input = document.querySelector('#id')
+    const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=fdff2360f3ab6fe1750a9be6ebab1eba&query=${input.value}`)
+    const res = await data.json()
+    console.log(res.results);
+    
+    res.results.forEach(element => {
+        card.innerHTML += `<div class = "info">
+                           <img src = "https://api.themoviedb.org/t/p/w500${element.poster_path} alt="">
+                           <p>${element.original_title}</p>
+                           <p>${element.release_date}</p>
+                           </div>
+                           `
+    });
 
 })
 
-    console.log(await (list(`https://api.themoviedb.org/3/search/movie?api_key=fdff2360f3ab6fe1750a9be6ebab1eba&query=${movie.value}`)));
-
+   
 
 
