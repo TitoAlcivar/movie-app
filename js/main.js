b6b4d935ac0810e639de765fb6afc375a598d767
TitoAlcivar <tjal2002@outlook.esm>
2023-06-22 21:25:01 +0200

    Avances

--- js/main.js
+++ js/main.js
@@ -1,42 +1,50 @@
 const form = document.querySelector('#movieapp')
 const card = document.querySelector('#movie')
-const popular = document.querySelector('#moviepopular')
 
+const getData = async (url) => {
+    const data = await fetch(url)
+    const response = await data.json()
+    return response
+}
 
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const input = document.querySelector('#id')
-    const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=fdff2360f3ab6fe1750a9be6ebab1eba&query=${input.value}`)
-    const res = await data.json()
-    console.log(res.results);
-    
-    res.results.forEach(element => {
-        card.innerHTML += `<div class = "info">
-        <img src = "https://image.tmdb.org/t/p/w500/${element.poster_path}" alt="">
-        <p>${element.original_title}</p>
-        <p>${element.release_date}</p>
-        <button class = "favorite">Favorite</button>
-        </div>
-        `
-    });
-    
-})
 
 const moviePopular = async () => {
-    const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=fdff2360f3ab6fe1750a9be6ebab1eba');
-    const data = await response.json();
+    const data = await getData('https://api.themoviedb.org/3/movie/popular?api_key=fdff2360f3ab6fe1750a9be6ebab1eba');
+    print(data.results)
+}
+moviePopular()
+
+form.addEventListener('submit', async (event) => {
+
+    event.preventDefault();
+    const input = document.querySelector('#id').value
+    Api(input)
+
+})
+
+const Api = async (input) => {
+    card.innerHTML = ''
+    const data = await getData(`https://api.themoviedb.org/3/search/movie?api_key=fdff2360f3ab6fe1750a9be6ebab1eba&query=${input}`)
     console.log(data.results);
-    
-    data.results.forEach(element => {
-      popular.innerHTML +=`<div class = "popular">
-                           <img src = "https://image.tmdb.org/t/p/w500${element.poster_path}"alt="">
+    print(data.results)
+
+}
+
+const print = (movies) => {
+
+    movies.forEach(element => {
+
+        card.innerHTML += `
+                           <div class = "info">
+                           <img src = "https://image.tmdb.org/t/p/w500/${element.poster_path}" alt="">
                            <p>${element.original_title}</p>
                            <p>${element.release_date}</p>
                            <button class = "favorite">Favorite</button>
-                           </div>`
-                    })
-}
-moviePopular()
+                          </div>`
+
+    })
+};
+
 
 
 
